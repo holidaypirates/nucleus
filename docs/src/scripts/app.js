40306f5216217a534f89d2c0f8496ce7022a1648
Michael Seibt <m.seibt@holidaypirates.com>
2016-08-15 16:53:51 +0200

    Publish docs

--- docs/src/scripts/app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-require('./preview-draggable');
-require('./code-highlight');
--- docs/src/scripts/preview-draggable.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require('jquery-ui/ui/widgets/mouse');
-require('jquery-ui/ui/widgets/resizable');
-require('touchpunch');
-
-$('.split-pane__preview').resizable({
-  handles: 'e'
-});
